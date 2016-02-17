d5c8938dbcf056ddef606d1cbf920828d0d33a0b
Toni de la Fuente <toni@blyx.com>
2016-02-17 00:18:36 -0500

    Create isolate.sh

---
+++ isolate.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# My system IP/set ip address of server
+OFFICE_IP=“YOUR.PUBLIC.IP.ADDR"
+# Flushing all rules
+iptables -F
+iptables -X
+# Setting default filter policy
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
+iptables -P FORWARD DROP
+# Allow unlimited traffic on loopback
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+# Allow incoming ssh only
+iptables -A INPUT -p tcp -s $OFFICE_IP  --sport 513:65535 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp -d $OFFICE_IP --sport 22 --dport 513:65535 -m state --state ESTABLISHED -j ACCEPT
+# make sure nothing comes or goes out of this box
+iptables -A INPUT -j DROP
+iptables -A OUTPUT -j DROP
