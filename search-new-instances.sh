743576eaabf19ca886f40092774ef969b64f9fb2
Toni de la Fuente <toni@blyx.com>
2016-02-17 22:04:46 -0500

    Update search-new-instances.sh

--- search-new-instances.sh
+++ search-new-instances.sh
@@ -3,7 +3,7 @@
 #date format YYYY-MM-DD
 DATE_SINCE=2016-02-01
 
-# list of all regions at Feb 17th 2016
+# list of all available regions by Feb 17th 2016
 REGIONS="us-east-1 us-west-2 us-west-1 eu-west-1 eu-central-1 ap-southeast-1 ap-northeast-1 ap-southeast-2 ap-northeast-2 sa-east-1"
 
 for i in $REGIONS;
